848fff21aa6ee588a886c43b82c063bd98b281d3
washucode <estherw8525@gmail.com>
2020-03-16 09:40:00 +0000

    add db connection

---
+++ Database/db.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$serverName = "DESKTOP-33TD627";
+
+$connectioninfo= array("Database"=>"New Muthokinju Hardware Ltd Database","UID"=>"esther","PWD"=>"esther");
+
+$conn = sqlsrv_connect($serverName,$connectioninfo);
+
+
+
+?>
