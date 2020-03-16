6523af4696506047b1e16402be28a28fc06ff08e
washucode <estherw8525@gmail.com>
2020-03-16 22:38:00 +0000

    create connection using class

--- Database/db.php
+++ Database/db.php
@@ -1,11 +1,20 @@
 <?php
 
 
-$serverName = "DESKTOP-33TD627";
-
-$connectioninfo= array("Database"=>"New Muthokinju Hardware Ltd Database","UID"=>"esther","PWD"=>"esther");
-
-$conn = sqlsrv_connect($serverName,$connectioninfo);
+class Database{
+	
+	private $serverName  = 'DESKTOP-33TD627';
+    private $connectioninfo = array("Database"=>"New Muthokinju Hardware Ltd Database","UID"=>"esther","PWD"=>"esther");
+    public function getConnection(){		
+		$conn = sqlsrv_connect($this->serverName,$this->connectioninfo);
+		if($conn){
+			
+			return $conn;
+		} else {
+           echo "connection error";
+		}
+    }
+}
 
 
 
